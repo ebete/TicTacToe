ac268d99851be0678cd05ff100dac99ec78915c0
ebete <thomgriffioen@gmail.com>
2015-02-17 16:41:37 +0100

    Implementation of a learning bot

---
+++ src/nl/han/bots/BotLearning.java
@@ -0,0 +1,20 @@
+package nl.han.bots;
+
+import nl.han.tictactoe.TicTacToe;
+
+/**
+ * Bot for Tic Tac Toe. This bot learns from played rounds.
+ *
+ * @author Thom Griffioen
+ */
+public class BotLearning implements Bot {
+    @Override
+    public void doMove(TicTacToe game) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Bot learning";
+    }
+}
